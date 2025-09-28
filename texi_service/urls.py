c6a22e2346e31039ec2be4059aecfe91ba9e8ded
Olexii Babii <babiy0617@gmail.com>
2025-09-28 22:38:12 +0300

    fix_1

--- texi_service/urls.py
+++ texi_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for text_service project.
+URL configuration for texi_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
